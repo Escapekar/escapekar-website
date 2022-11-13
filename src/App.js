27b613c13e433d022c13e5b327d91fe8d9c633c4
Udit Malhotra <udit.malhotra@spenda.co>
2022-11-13 22:21:30 +0530

    main

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 const App = () => {
   return (
     <div className="App flex flex-col h-full box-border bg-[#f1f1f1]">
-      <Router>
+      <Home />
+      {/* <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/privacy-policy" element={<PrivacyPolicy />} />
           <Route exact path="/terms-of-use" element={<TermsOfUse />} />
         </Routes>
-      </Router>
+      </Router> */}
     </div>
   );
 }
